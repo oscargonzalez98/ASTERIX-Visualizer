d9ac9a18aa94b45af9fce83370c5d4525c962b10
oscargonzalez98 <oscar.go.martin@gmail.com>
2025-04-08 15:12:52 +0200

    Addedd GMapControl based on WindowsForms, which includes GMapOverlay

--- ASTERIX_Visualizer/DataModelLibrary/obj/Debug/net8.0-windows/DataModelLibrary.AssemblyInfo.cs
+++ ASTERIX_Visualizer/DataModelLibrary/obj/Debug/net8.0-windows/DataModelLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataModelLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b087e71089597860d789733e51e250dbb5c7160")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5aa81cb7550e4b4a1e9aa3679ebc0392398b003c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataModelLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataModelLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ASTERIX_Visualizer/MapManagementLibrary/CustomMapView.cs
+++ ASTERIX_Visualizer/MapManagementLibrary/CustomMapView.cs
@@ -3,18 +3,25 @@ using GMap.NET;
 using GMap.NET.WindowsPresentation;
 using System.Windows;
 using System.Windows.Input;
+using DataManagementLibrary;
+using DataModel;
+using GMap.NET.WindowsForms.Markers;
+using System.Windows.Forms;
 
 namespace MapManagementLibrary
 {
-    public class CustomMapView : GMapControl
+    public class CustomMapView : GMap.NET.WindowsForms.GMapControl
     {
+        private GMap.NET.WindowsForms.GMapOverlay beaconsOverlay;
+
         public CustomMapView()
         {
             // Custom initialization or default settings
-            this.Loaded += CustomGMapControl_Loaded;
+            CustomGMapControl_LoadMap();
+            CustomGMapControl_LoadBeacons();
         }
 
-        private void CustomGMapControl_Loaded(object sender, RoutedEventArgs e)
+        private void CustomGMapControl_LoadMap()
         {
             this.MapProvider = GMapProviders.GoogleMap;
             GMaps.Instance.Mode = AccessMode.ServerAndCache;
@@ -26,7 +33,49 @@ namespace MapManagementLibrary
 
             this.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
             this.CanDragMap = true;
-            this.DragButton = MouseButton.Left;
+
+            // Fix for CS0266: Explicitly convert System.Windows.Input.MouseButton to System.Windows.Forms.MouseButtons
+            this.DragButton = ConvertMouseButton(MouseButton.Left);
+        }
+
+        private void CustomGMapControl_LoadBeacons()
+        {
+            beaconsOverlay = new GMap.NET.WindowsForms.GMapOverlay("beacons");
+
+            string beaconsDataPath = @"C:\Users\oscar\Documents\C# Projects\ASTERIX-Visualizer\ASTERIX_Visualizer\src\Mapas\Peninsula_Fijos.map";
+
+            IDataLoader<List<Beacon>> beaconsDataLoader = new BeaconsDataLoader();
+            List<Beacon> beacons = beaconsDataLoader.loadData(beaconsDataPath);
+
+            foreach (Beacon beacon in beacons)
+            {
+                double lat = beacon.getCoordinates().getLatitude();
+                double lon = beacon.getCoordinates().getLongitude();
+
+                // Use GMap.NET.WindowsForms.GMapMarker instead of GMap.NET.WindowsPresentation.GMapMarker
+                var marker = new GMap.NET.WindowsForms.Markers.GMarkerGoogle(
+                    new PointLatLng(lat, lon),
+                    GMarkerGoogleType.red_dot
+                );
+
+                beaconsOverlay.Markers.Add(marker);
+            }
+
+            this.beaconsOverlay.IsVisibile = true; // Ensure the overlay is visible
+
+            this.Overlays.Add(beaconsOverlay); // Add the overlay to the map
+        }
+
+        // Helper method to convert System.Windows.Input.MouseButton to System.Windows.Forms.MouseButtons
+        private System.Windows.Forms.MouseButtons ConvertMouseButton(MouseButton button)
+        {
+            return button switch
+            {
+                MouseButton.Left => System.Windows.Forms.MouseButtons.Left,
+                MouseButton.Right => System.Windows.Forms.MouseButtons.Right,
+                MouseButton.Middle => System.Windows.Forms.MouseButtons.Middle,
+                _ => System.Windows.Forms.MouseButtons.None,
+            };
         }
     }
 }
